80b16a07c6c356a148b6281b5c1d89fd5e99bdd8
Celine-Nova <cocotte243@gmail.com>
2022-07-02 17:33:20 +0200

    création projet backend

---
+++ backend-crm/src/test/java/com/mycrm/backendcrm/BackendCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycrm.backendcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
